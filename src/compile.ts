600640a1911eff2c2405542ef061219dd0805732
Ezequiel Falduto <efalduto@gmail.com>
2021-03-24 14:47:03 -0300

    Base boilerplate with ts + eslint + solc@0.4

---
+++ src/compile.ts
@@ -0,0 +1,11 @@
+import { resolve } from 'path'
+import { readFileSync } from 'fs';
+
+import { compile } from 'solc';
+
+const contractPath = resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = readFileSync(contractPath, {
+  encoding: 'utf-8',
+});
+
+export default compile(source, 1); 
